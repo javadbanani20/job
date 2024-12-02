0e685e62c3b7f95f1df6b7056fec6de77c76034c
javadbanani20 <javadbanani79@gmail.com>
2024-12-02 22:25:40 +0330

    for exampla second commit

---
+++ app/Http/Controllers/JobController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+use Illuminate\Http\Request;
+
+class JobController extends Controller
+{
+    public function index(){
+        $jobs = Job::with('admin')->paginate(5);
+        return view('jobs.index',[
+            'jobs' => $jobs
+        ]);
+    }
+    public function create(){
+        return view('jobs.create');
+    }
+    public function show(Job $job){
+        return view('jobs.show',['job' => $job]);
+    }
+    public function store(){
+        request()->validate([
+            'title' => ['required', 'min:3' , 'max:25'],
+            'salary' => 'required',
+        ]);
+        Job::create([
+            'title' => request('title'),
+            'salary' => request('salary'),
+            'employer_id' => 1,
+        ]);
+        return redirect('/jobs');
+    }
+    public function edit(Job $job){
+        return view('jobs.edit',['job' => $job]);
+    }
+    public function update(Job $job){
+        //validate
+        request()->validate([
+            'title' => ['required', 'min:3' , 'max:40'],
+            'salary' => 'required',
+        ]);
+
+        $job->update([
+            'title' => request('title'),
+            'salary' => request('salary'),
+        ]);
+        //redirect
+        return redirect('/jobs/'.$job->id);
+    }
+    public function destroy(Job $job){
+        $job->delete();
+        return redirect('/jobs');
+    }
+}
